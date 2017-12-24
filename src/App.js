cf60948de861960fdc2bd5955cb250f12498d344
Chris Umeki <ctu3@cornell.edu>
2017-12-23 19:33:55 -0600

    added BigRedGifts homepage's styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+//import './all.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
+
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="logo">
+          <img id = "logo" src={logo} className="App-logo" alt="logo" />
+          <h1>Big Red Gifts<br/>Feedback Form</h1>
+        </div>
+      </div>
       </div>
     );
   }
